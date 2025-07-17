826bd282aa54748484788779bf9495fe9f1c93e8
Mohamed <mohamed.aboulsoud@fci.sohag.edu.eg>
2025-07-17 05:09:30 +0300

    Add Feature to Delete Contacts by Swiping

--- SimpleContactsTask/Controllers/ContactsViewController.swift
+++ SimpleContactsTask/Controllers/ContactsViewController.swift
@@ -12,11 +12,11 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
 
     @IBOutlet weak var addContactButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
-    
+
     override var preferredStatusBarStyle: UIStatusBarStyle{
-       return .lightContent
+        return .lightContent
     }
-    
+
     func UILoad () {
         addContactButton.layer.shadowColor = UIView().tintColor.cgColor
         addContactButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
@@ -25,34 +25,34 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
         addContactButton.layer.masksToBounds = false
         addContactButton.layer.cornerRadius = 4.0
     }
-    
+
     var contacts: [Contact] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         UILoad()
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "openContactDetails") {
             let contactDetailsController = segue.destination as! ContactDetailsViewController
-            
+
             if let index = tableView.indexPathForSelectedRow?.row {
                 contactDetailsController.selectedContactId = contacts[index].objectID
             }
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         fetchContacts()
     }
-    
+
     func fetchContacts() {
         let contactManager = ContactCoreDataManager()
-        
+
         do {
             contacts = try contactManager.getAll()
             tableView.reloadData()
@@ -60,20 +60,20 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
             ErrorAlert.handleError(self, message: "Error occurs fetching contacts")
         }
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let contactCell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath) as! ContactTableViewCell
-        
+
         let currentContact = contacts[indexPath.row]
-        
+
         contactCell.name.text = currentContact.name
         contactCell.phoneNumber.text = currentContact.phoneNumber
 
@@ -82,11 +82,34 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
         }
         return contactCell
     }
-    
+
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCell.EditingStyle.delete) {
+            // delete data and row
+            do {
+                let contactManager = ContactCoreDataManager()
+                try contactManager.delete(id: self.contacts[indexPath.row].objectID)
+            } catch {
+                ErrorAlert.handleError(self, message: "Error occurs while deleting contact")
+            }
+
+            
+
+            contacts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+
 }
 
 extension UINavigationController {
-   open override var preferredStatusBarStyle: UIStatusBarStyle {
-       return topViewController?.preferredStatusBarStyle ?? .lightContent
-   }
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        return topViewController?.preferredStatusBarStyle ?? .lightContent
+    }
 }
